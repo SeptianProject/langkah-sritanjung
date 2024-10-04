7bf458278ec6450f69e5d599c9ac0d6a30a49176
Septian Asropik <septianuwoe75@gmail.com>
2024-10-04 19:52:01 +0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HomePage from "./components/pages/HomePage"
+
+const App = () => {
+  return (
+    <section className='container max-w-screen-xl min-h-screen'>
+      <HomePage />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    const [isMobile, setIsMobile] = useState<boolean>(false)
+
+    return (
+        <nav>
+            <div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
